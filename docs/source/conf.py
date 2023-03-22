e5e214d8bac1b6a3f394dc35d104f7e70543d874
Victor Näslund <victor@sunet.se>
2023-03-22 09:25:30 +0100

    setting up autogenerated docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,11 +13,10 @@ from sphinx.ext.apidoc import main as sphinx_apidoc
 # sys.path.insert(0, os.path.abspath('../src/pkcs11_ca_service/'))
 # print(sys.path)
 
-print(os.listdir("."))
-
 # Auto generate docs with sphinx api docs
-if sphinx_apidoc(['--implicit-namespaces', '-o', 'docs/source/', 'src/pkcs11_ca_service/']) != 0:
+if sphinx_apidoc(['--implicit-namespaces', '-o', '.', '../../src/pkcs11_ca_service/']) != 0:
     print("ERROR: Could not auto generate docs")
+    sys.exit(1)
 
 project = 'PKCS11CA'
 copyright = '2023, SUNET'
