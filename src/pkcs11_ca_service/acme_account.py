5fe529b4190f01d9aa61781ab976388dbf39f6b8
Victor Näslund <victor@sunet.se>
2023-03-22 10:41:23 +0100

    testing rest style docstrings

--- src/pkcs11_ca_service/acme_account.py
+++ src/pkcs11_ca_service/acme_account.py
@@ -3,11 +3,9 @@ from typing import Dict, Union, Any, List
 import json
 
 from .base import DataClassObject, DataBaseObject, InputObject
-
 from .asn1 import from_base64url, to_base64url
 
 # FIXME use enum instead of str when appropriate
-
 # FIXME add date_created field
 
 
@@ -41,12 +39,13 @@ class AcmeAccount(DataClassObject):
     db_unique_fields = ["id", "public_key_pem"]
 
     def contact_as_list(self) -> List[str]:
-        """Get the contacts for this account.
-        They are stored in base64url
+        """Get the contacts for this account as a json list
+        They are stored in base64url in the database.
 
-        Returns:
-        List[str]
+        :return: The json list
+        :rtype: list[str]
         """
+
         ret: List[str] = json.loads(from_base64url(self.contact))
         return ret
 
@@ -54,11 +53,11 @@ class AcmeAccount(DataClassObject):
 def contact_from_payload(payload: Dict[str, Any]) -> str:
     """Encode the acme new account contacts to base64url
 
-    Parameters:
-    payload (Dict[str, Any): The payload part pf the acme JWS.
+    :param payload: The payload part pf the acme JWS.
+    :type payload: dict[str, Any]
 
-    Returns:
-    str
+    :return: The json list contacts as a base64url encoded string
+    :rtype: str
     """
 
     contacts: List[str] = []
