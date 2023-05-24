0bdf1bf647de738b5deb646ae3ed685020ebed24
Victor Näslund <victor@sunet.se>
2023-05-24 18:59:51 +0200

    updated API spec for pdfsign

--- src/pkcs11_ca_service/pdfsign.py
+++ src/pkcs11_ca_service/pdfsign.py
@@ -1,32 +1,39 @@
 """pdfsign module, FastAPI runs from here"""
+import base64
 import os
 import secrets
 import subprocess
 
 from fastapi import FastAPI, HTTPException, Request, Response
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
 
 # Create fastapi app
 # Disable swagger and docs endpoints for now
 app = FastAPI(docs_url=None, redoc_url=None)
 
 
+class PDFInput(BaseModel):
+    """Class to represent PDF and metadata matching from HTTP post data"""
+
+    transaction_id: str
+    pdf_b64_data: str
+
+
 @app.post("/pdfsign01")
-async def post_pdf(request: Request) -> Response:
+async def post_pdf(pdf_input: PDFInput) -> JSONResponse:
     """PDF fixme"""
 
-    media_type = "application/pdf"
-
-    content_type = request.headers.get("Content-type")
-    if content_type is None or content_type != media_type:
-        return Response(status_code=400, content=b"0", media_type=media_type)
-
-    data = await request.body()
     print("Received a PDF file")
 
+    if len(pdf_input.pdf_b64_data) < 3:
+        raise ValueError("Invalid PDF")
+
+    pdf_bytes = base64.b64decode(pdf_input.pdf_b64_data, validate=True)
     filename = str(secrets.randbits(128))
 
     with open(f"{filename}.pdf", "wb") as f_data:
-        f_data.write(data)
+        f_data.write(pdf_bytes)
 
     print("Trying to sign the PDF")
     subprocess.check_call(
@@ -42,14 +49,14 @@ async def post_pdf(request: Request) -> Response:
     with open(f"signed_{filename}.pdf", "rb") as f_data:
         signed_pdf_bytes = f_data.read()
 
+    signed_pdf_b64 = base64.b64encode(signed_pdf_bytes).decode("utf-8")
+
     print("Removing temporary disk files")
     os.remove(f"signed_{filename}.pdf")
     os.remove(f"{filename}.pdf")
 
     print("Sending the signed PDF back to client")
 
-    # curl http://ca:8005/timestamp01 -H 'Content-Type: application/timestamp-query' -s -S --data-binary "@ts_req.tsq" -o "ts_req.tsr"
-    # openssl ts -reply -in ts_req.tsr -token_out 2> /dev/null | openssl pkcs7 -inform der -print_certs 2> /dev/null | sed -n '/-----BEGIN/,/-----END/p' > timestamp_chain.pem
-    # cat timestamp_chain.pem >> ts_chain.pem
-
-    return Response(status_code=200, content=signed_pdf_bytes, media_type=media_type)
+    return JSONResponse(
+        status_code=200, content={"transaction_id": pdf_input.transaction_id, "signed_pdf_b64_data": signed_pdf_b64}
+    )
